e11e0ecc6874d29095a5723b99f382df30c7a982
kanta <kanta510g@outlook.jp>
2022-12-09 19:12:52 +0900

    create convolution layer and im2col function

---
+++ layers.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+class Convolution:
+    def __init__(self, prev_layer_size, layer_size):
+        self.prev_layer_size = prev_layer_size
+        self.layer_size = layer_size
+        self.filter_size = 3
+        self.stride = 1
+        self.out_h = (prev_layer_size[1] - self.filter_size) // self.stride + 1
+        self.out_w = (prev_layer_size[2] - self.filter_size) // self.stride + 1
+        
+        # He初期化　重み：(レイヤーサイズ, チャンネル数, フィルターサイス, フィルターサイズ)
+        self.W = 0.1 * np.random.randn(layer_size, prev_layer_size[0], self.filter_size, self.filter_size)
+        self.b = np.zeros((layer_size))
+        
+    # def forward(self):
+        
+    def im2col(self, A_prev):
+        col = np.zeros((self.layer_size, self.prev_layer_size[0], self.filter_size, self.filter_size, self.out_h, self.out_w))
+        print(col.shape)
+        
+        for y in range(self.filter_size):
+            y_max = y + self.stride*self.out_h
+            for x in range(self.filter_size):
+                x_max = x + self.stride*self.out_w
+                col[:, :, y, x, :, :] = A_prev[:, y:y_max:self.stride, x:x_max:self.stride]
+                
+        col = col.transpose(0, 4, 5, 1, 2, 3).reshape(self.layer_size * self.out_h * self.out_w, -1)
+        return col
+    
+# class Linear:
+#     def __init__(self):
+        
+#     def forward(self):
+        
+        
+# class Pooling:
+#     def __init__(self):
+    
+#     def forward(self):
+        
