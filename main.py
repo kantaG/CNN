04484b3c4569e9da50d4fba993486ff9024682c5
kanta <kanta510g@outlook.jp>
2022-12-10 04:35:33 +0900

    create test layers

--- main.py
+++ main.py
@@ -2,45 +2,90 @@ import numpy as np
 import emnist
 import layers
 
-test = layers.Convolution([2, 10, 10], 2)
-
-test_pool = layers.Pooling([2, 10, 10])
-
-test_flat = layers.Flatten
-
-test_lin = layers.Linear([1, 10], 20)
+t_conv = layers.Convolution([2, 10, 10], 2)
+t_relu = layers.ReLU
+t_pool = layers.Pooling([2, 8, 8])
+t_flat = layers.Flatten
+t_line = layers.Linear([1, 32], 10)
 
 data1 = [[i+j for i in range(10)] for j in range(10)]
 
 data2 = [[i*10+j*10 for i in range(10)] for j in range(10)]
+data = np.array([data1, data2])
 
-data3 = [i for i in range(10)]
-data = np.array([data3])
-
+print("origin")
 print(data.shape)
 print(data)
 
-result = test_lin.forward(data)
-print(result)
-print(result.shape)
+data = t_conv.forward(data)
 
-# print(test_flat.forward(data))
-# print(test_flat.forward(data).shape)
+print("conv")
+print(data.shape)
+print(data)
 
-# result = test_pool.forward(data)
+data = t_relu.forward(data)
+
+print("relu")
+print(data.shape)
+print(data)
+
+data = t_pool.forward(data)
+
+print("pool")
+print(data.shape)
+print(data)
+
+data = t_flat.forward(data)
+
+print("flat")
+print(data.shape)
+print(data)
+
+data = t_line.forward(data)
+
+print("line")
+print(data.shape)
+print(data)
+
+# test = layers.Convolution([2, 10, 10], 2)
+
+# test_pool = layers.Pooling([2, 10, 10])
+
+# test_flat = layers.Flatten
+
+# test_lin = layers.Linear([1, 10], 20)
+
+# data1 = [[i+j for i in range(10)] for j in range(10)]
+
+# data2 = [[i*10+j*10 for i in range(10)] for j in range(10)]
+
+# data3 = [i for i in range(10)]
+# data = np.array([data3])
+
+# print(data.shape)
+# print(data)
+
+# result = test_lin.forward(data)
 # print(result)
 # print(result.shape)
 
+# # print(test_flat.forward(data))
+# # print(test_flat.forward(data).shape)
 
-# # print(test.W)
+# # result = test_pool.forward(data)
+# # print(result)
+# # print(result.shape)
 
-# # print(test.im2col(data).shape)
-# # print(test.im2col(data))
 
-# result = test.forward(data)
-# print(result)
-# print(result.shape)
+# # # print(test.W)
 
-# relu = layers.ReLU
+# # # print(test.im2col(data).shape)
+# # # print(test.im2col(data))
 
-# print(relu.forward(result))
+# # result = test.forward(data)
+# # print(result)
+# # print(result.shape)
+
+# # relu = layers.ReLU
+
+# # print(relu.forward(result))
