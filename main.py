4e61877288fe93483c47cabf8ee156aa4e4adadb
kanta <kanta510g@outlook.jp>
2022-12-10 04:18:05 +0900

    tests

--- main.py
+++ main.py
@@ -2,17 +2,45 @@ import numpy as np
 import emnist
 import layers
 
-test = layers.Convolution([2, 10, 10], 1)
+test = layers.Convolution([2, 10, 10], 2)
+
+test_pool = layers.Pooling([2, 10, 10])
+
+test_flat = layers.Flatten
+
+test_lin = layers.Linear([1, 10], 20)
 
 data1 = [[i+j for i in range(10)] for j in range(10)]
 
 data2 = [[i*10+j*10 for i in range(10)] for j in range(10)]
-data = np.array([data1, data2])
+
+data3 = [i for i in range(10)]
+data = np.array([data3])
 
 print(data.shape)
 print(data)
 
-# print(test.W)
+result = test_lin.forward(data)
+print(result)
+print(result.shape)
 
-print(test.im2col(data).shape)
-print(test.im2col(data))
+# print(test_flat.forward(data))
+# print(test_flat.forward(data).shape)
+
+# result = test_pool.forward(data)
+# print(result)
+# print(result.shape)
+
+
+# # print(test.W)
+
+# # print(test.im2col(data).shape)
+# # print(test.im2col(data))
+
+# result = test.forward(data)
+# print(result)
+# print(result.shape)
+
+# relu = layers.ReLU
+
+# print(relu.forward(result))
